82ce2ef439de12c12d866e17450497e455923796
Hugo CHASTEL <hugo.chastel@orange.com>
2020-02-05 15:43:36 +0100

    Added exception handling when the arduino isn't connected

--- web/views.py
+++ web/views.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from serial import SerialException
 
 from utils.arduino_wrapper import ArduinoWrapper
 
@@ -18,8 +19,11 @@ def hello_world():
 def send_message():
     message = request.form['message']
     print(message)
-    ArduinoWrapper().send_msg(message)
-    return message, 200
+    try:
+        ArduinoWrapper().send_msg(message)
+        return message, 200
+    except SerialException:
+        return "Arduino seem's to be NOT connected", 500
 
 
 if __name__ == "__main__":

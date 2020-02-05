31a11e30f782e44fdc82a830170d8b7611538a7b
sirambd <sirambd.tsone@gmail.com>
2020-02-05 15:14:06 +0100

    init page

--- web/views.py
+++ web/views.py
@@ -1,11 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html',
+                           user_name='TP_IOT',
+                           description="Bienvenu dans notre page pour tester le raspbery",
+                           blur=False,
+                           page_title="index")
 
 
 @app.route('/sendMessage', methods=['POST'])
